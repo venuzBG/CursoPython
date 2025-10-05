fb45ebfabfc935dffad899d362f2d5322946439e
venuzBG <sebas_alejandro@outlook.es>
2025-10-05 15:23:45 -0500

    Final_Unidad_1

---
+++ Unidad_1/Variables/constantes.py
@@ -0,0 +1,15 @@
+print("")
+print("*** Constantes en python ***")
+
+# Constantes en python no deben ser modificada si esta en mayuscula
+PI = 3.1416
+GRAVEDAD = 9.81
+
+print("Valor de PI:", PI)
+print("Valor de GRAVEDAD:", GRAVEDAD)
+print("")
+
+NOMBRE_BASE_DATOS = "mi_base_datos.db"
+print("Nombre de la base de datos:", NOMBRE_BASE_DATOS)
+print("")
+print("*** Fin del programa ***")
